72c54f48d0727afc055bcd6885915242e5ffc181
Joseph Sommer <jsommer1@stanford.edu>
2019-12-06 05:27:33 -0800

    changed name to save model as

--- base_model_train_big.py
+++ base_model_train_big.py
@@ -114,7 +114,7 @@ history = model.fit(X_train, years_train,
                     validation_data=(X_test, years_test),
                     batch_size=my_batch_size)
 
-model.save('my_NN_bigger_data_1.h5')
+model.save('my_NN_bigger_data_2.h5')
 
 """
 Model 1: batch size 25, 75 epochs

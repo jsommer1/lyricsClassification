926322b61fd10df611f74d1e0973c85dfa565930
Joseph Sommer <jsommer1@stanford.edu>
2019-12-07 02:17:27 -0800

    tweak bidi 6

--- bidirectional_train.py
+++ bidirectional_train.py
@@ -182,9 +182,10 @@ for j in range(N_test):
 
 input_1 = layers.Input((1,max_len))
 #LSTM 
-dense_1 = Bidirectional(LSTM(10, 
+bidi_1 = Bidirectional(LSTM(10, 
                              activation='tanh',
-                             input_shape=(1,max_len)))(input_1)
+                             input_shape=(1,max_len),
+                             return_sequences=True))(input_1)
 #drop_1 = Dropout(0.5)(bidi_1)
 #bidi_2 = Bidirectional(LSTM(10, 
 #                             activation='tanh',
@@ -192,10 +193,10 @@ dense_1 = Bidirectional(LSTM(10,
 #drop_2 = Dropout(0.5)(bidi_2)
 #bidi_3 = Bidirectional(LSTM(10, 
 #                             activation='tanh'))(drop_2)
-#drop_3 = Dropout(0.5)(bidi_3)
-#
-#dense_1 = layers.Dense(10, 
-#                        activation='tanh')(drop_3)
+drop_3 = Dropout(0.5)(bidi_1)
+
+dense_1 = layers.Dense(10, 
+                        activation='tanh')(drop_3)
 
 
 att = layers.Dense(10,input_dim=10)(dense_1)
